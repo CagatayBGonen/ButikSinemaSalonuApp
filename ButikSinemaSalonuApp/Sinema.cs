dc22bcee3040e11b1fd9c63b704a696e03b983ca
Cagatay <cagataybayramgonen@gmail.com>
2022-07-31 11:53:08 +0300

    Add project files.

---
+++ ButikSinemaSalonuApp/Sinema.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ButikSinemaSalonuApp
+{
+    class Sinema
+    {
+        public string MovieName;
+        public int Capacity;
+        public double FullTicketPrice;
+        public double HalfTicketPrice;
+        public int SumOfFullTicketNumber;
+        public int SumOfHalfTicketNumber;
+        public double Ciro;
+        public int EmptySeatNumber;
+    }
+}
---
+++ ButikSinemaSalonuApp/SinemaApp.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ButikSinemaSalonuApp
+{
+    public class SinemaApp
+    {
+        public SinemaApp()
+        {
+            Kurulum();
+            Menu();
+            Secim();
+        }
+        static void Kurulum()
+        {
+            Console.WriteLine("---Butik Sinema Salonu---");
+            Console.Write("Movie name: ");
+            string movieName = Console.ReadLine();
+            Console.Write("Capacity: ");
+            int capacity = int.Parse(Console.ReadLine());   
+            Console.Write("Full Ticket Price: ");
+            double fullTicketPrice = double.Parse(Console.ReadLine());
+            Console.Write("Half Ticket Pice: ");
+            double halfTicketPrice = double.Parse(Console.ReadLine());
+        }
+        static void Menu()
+        {
+            Console.WriteLine("1-Sell Ticket(S)" +
+                "\n2-Return Ticket(R)" +
+                "\n3-Statue Information(I)" +
+                "\n4-Exit(X)");
+        }
+        static void Secim()
+        {
+            while (true)
+            {
+                Console.Write("Your Selection: ");
+                string selection = Console.ReadLine();
+                switch (selection)
+                {
+                    case "S":
+                        SellTicket();
+                        break;
+                    case "1":
+                        SellTicket();
+                        break;
+                    case "R":
+                        ReturnTicket();
+                        break;
+                    case "2":
+                        ReturnTicket();
+                        break;
+                    case "I":
+                        StatueInformation();
+                        break;
+                    case "3":
+                        StatueInformation();
+                        break;
+                    case "X":
+                        return;                       
+                    case "4":
+                        return;                        
+                }
+            }
+        }
+
+        private static void StatueInformation()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ReturnTicket()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void SellTicket()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
